83223eb6c0644d7995ad44543026def9fbfc3006
emlag <edl926@g.harvard.edu>
2020-07-20 13:37:42 +0800

    add firebase

--- examsite/src/components/App.js
+++ examsite/src/components/App.js
@@ -5,6 +5,20 @@ import flipCard from "./flipCard";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import createQuestion from "./createQuestion";
 import homepage from "./homepage";
+import * as firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBBYjAy2nWmAyUzN54cS3wa9BwbuLHQoQI",
+    authDomain: "examprep-a775e.firebaseapp.com",
+    databaseURL: "https://examprep-a775e.firebaseio.com",
+    projectId: "examprep-a775e",
+    storageBucket: "examprep-a775e.appspot.com",
+    messagingSenderId: "1031338941170",
+    appId: "1:1031338941170:web:73f7f6a818f9eb8265ce6b",
+    measurementId: "G-3G98EFVWNB"
+  };
+
+firebase.initializeApp(firebaseConfig);
 
 class App extends Component {
     render() {
