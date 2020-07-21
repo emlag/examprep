64ddffa1b01f0df71410c041797f9e0756190a8b
emlag <edl926@g.harvard.edu>
2020-07-21 09:42:41 +0800

    removed flipcard

--- examsite/src/components/App.js
+++ examsite/src/components/App.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Guide from './Guide';
-import flipCard from "./flipCard";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
-import createQuestion from "./createQuestion";
+import CreateQuestion from "./CreateQuestion";
+import PracticePage from "./PracticePage";
 import homepage from "./homepage";
 import * as firebase from "firebase";
 
@@ -28,8 +28,8 @@ class App extends Component {
                     <Switch>
                         <Route path="/" exact component={homepage}/>
                         <Route path="/guide" exact component={Guide}/>
-                        <Route path="/practice" exact component={flipCard}/>
-                        <Route path="/create" exact component={createQuestion}/>
+                        <Route path="/practice" exact component={PracticePage}/>
+                        <Route path="/create" exact component={CreateQuestion}/>
                     </Switch>
                 </div>
             </Router>
---
+++ examsite/src/components/PracticePage.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class PracticePage extends Component {
+
+
+    render() {
+        return (
+            <div>
+                Hi
+            </div>
+        )
+    }
+}
+
+export default PracticePage;
