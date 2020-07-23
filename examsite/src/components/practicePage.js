f2064c74199ca39ce8c26e7a87d913ca783bf551
emlag <edl926@g.harvard.edu>
2020-07-23 13:48:06 +0800

    remove google API dependencies

--- examsite/src/components/practicePage.js
+++ examsite/src/components/practicePage.js
@@ -54,7 +54,6 @@ export default function practicePage() {
     //creating variables for the states
     const {topicOne, topicTwo, topicThree, topicFour, topicFive, topicSix, topicSeven} = state;
 
-
     return (
         <div>
             <Container>
