0d8d9929126e1eaff056bdcf349ee073d97c4005
emlag <edl926@g.harvard.edu>
2020-08-04 12:25:21 +0800

    show answers in modal and toggle button title

--- examsite/src/components/QuestionModal.js
+++ examsite/src/components/QuestionModal.js
@@ -8,17 +8,14 @@ const QuestionModal = (props) => {
     } = props;
 
     const [modal, setModal] = useState(false);
+    const [showAnswer, setShowAnswer] = useState(false);
     const [note, setNote] = useState("hi this is a note");
 
     const toggle = () => setModal(!modal);
+    const toggleAnswer = () => setShowAnswer(!showAnswer);
 
-    // useEffect(() => {
-    //     getNote();
-    // }, [note, modal])
-    //
-
-    function getImages(imagesArr) {
-        const allImageDivs = imagesArr.map((imageURL, idx) => {
+    function getImages(data) {
+        const allImageDivs = data.questionImageUrls.map((imageURL, idx) => {
             return(
                 <img src={imageURL} alt={idx} width="100%"/>
             )
@@ -27,15 +24,40 @@ const QuestionModal = (props) => {
         return allImageDivs;
     }
 
+    function getAnswers(data) {
+        const allImageDivs = data.answerImageUrls.map((imageURL, idx) => {
+            return(
+                <img src={imageURL} alt={idx} width="100%"/>
+            )
+        })
+
+        return allImageDivs;
+    }
+
+    function toShow(data) {
+        if (showAnswer){
+            return getAnswers(data)
+        }
+
+        return getImages(data);
+    }
+
+    function buttonTitle() {
+        const title = showAnswer ? "Question" : "Mark Scheme";
+
+        return title;
+    }
+
     return (
         <div>
             <a onClick={toggle} style={{color: '#2980b9', cursor:"pointer"}}><u>{props.title}</u></a>
             <Modal isOpen={modal} toggle={toggle} size="lg" style={{maxWidth: '1200px', width: '60%'}}>
                 {/*<ModalHeader toggle={toggle}>{props.teamName}</ModalHeader>*/}
                 <ModalBody>
-                    {getImages(props.data.questionImageUrls)}
+                    {toShow(props.data)}
                 </ModalBody>
                 <ModalFooter>
+                    <Button color="secondary" onClick={toggleAnswer}>{buttonTitle()}</Button>{' '}
                     <Button color="primary" onClick={toggle}>OK</Button>{' '}
                 </ModalFooter>
             </Modal>
