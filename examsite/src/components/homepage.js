903beb01fc4acaf134803b2b3b9fa4a8e7e55f55
Allyson Benecia Chan <allysonbc2020@student.cis.edu.hk>
2020-08-04 11:26:56 +0800

    merging problems

--- examsite/src/components/homepage.js
+++ examsite/src/components/homepage.js
@@ -8,6 +8,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 import * as cnst from "./Const";
+import PDF from "../../../IBCS Past Papers";
 
 const title = {
   textAlign: "center",
